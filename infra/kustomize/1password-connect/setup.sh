be815db9aafa164e3ac272c8740ea4bbc945354f
Chan Quan Ian <work@quanianitis.com>
2025-04-06 01:02:55 +0800

    minor: fix

--- infra/kustomize/1password-connect/setup.sh
+++ infra/kustomize/1password-connect/setup.sh
@@ -14,5 +14,5 @@ helm install connect 1password/connect -n 1password --set-file connect.credentia
 rm /tmp/1password-credentials.json
 
 kubectl create secret generic 1password \
-    --from-literal=secret=$(op read op://quanianitis.com/1password-server-token/token) \
+    --from-literal=token=$(op read op://quanianitis.com/1password-server-token/token) \
     --namespace external-secrets
